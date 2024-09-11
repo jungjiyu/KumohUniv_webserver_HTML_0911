091ad238a4f2ab4235883aa16853e9bcf62a10c4
jungjiyu <142137932+jungjiyu@users.noreply.github.com>
2024-09-11 15:49:19 +0900

    init

---
+++ html01/src/main/kotlin/kr/ac/kumoh/ce/s20231073/html01/Html01Application.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20231073.html01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Html01Application
+
+fun main(args: Array<String>) {
+	runApplication<Html01Application>(*args)
+}
---
+++ html01/src/test/kotlin/kr/ac/kumoh/ce/s20231073/html01/Html01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20231073.html01
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Html01ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
